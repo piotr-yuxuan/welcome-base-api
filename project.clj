9ba264c00635ed74f49d48eba7817f7f9e7ce017
Walter CI <piotr-yuxuan@users.noreply.github.com>
2022-07-07 06:47:32 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -23,7 +23,7 @@
                  [cheshire "5.11.0"]
 
                  ;; Kafka and avro
-                 [org.apache.kafka/kafka-clients "7.1.2-ce"]
+                 [org.apache.kafka/kafka-clients "7.2.0-ce"]
                  [org.apache.avro/avro "1.11.0"]
                  [io.confluent/kafka-avro-serializer "7.1.2"]
                  [piotr-yuxuan/slava "0.33.0" :exclusions [riddley]] ; Avro record manipulation for Clojure
