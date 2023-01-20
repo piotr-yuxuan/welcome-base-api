836ccd8a8e8bb7889e369c7319980a77127af617
Walter CI <piotr-yuxuan@users.noreply.github.com>
2023-01-20 18:41:32 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -65,7 +65,7 @@
                                   [com.clojure-goes-fast/clj-async-profiler "1.0.3"] ; Sampling CPU and HEAP profiler for Clojure featuring AsyncGetCallTrace + perf_events
                                   [fipp "0.6.26"] ; Fast Idiomatic Pretty Printer for Clojure
                                   ]}
-             :kaocha {:dependencies [[lambdaisland/kaocha "1.73.1175"]]}
+             :kaocha {:dependencies [[lambdaisland/kaocha "1.75.1190"]]}
              :uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                                   "-Dclojure.compiler.direct-linking=true"]}}
