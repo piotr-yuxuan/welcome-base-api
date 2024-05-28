23dfbac911cb2ba6ef4f2e05c25e62263051170e
Walter CI <piotr-yuxuan@users.noreply.github.com>
2024-05-28 18:39:19 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -12,15 +12,15 @@
   :dependencies [;; Core business domain
 
                  ;; HTTP
-                 [http-kit "2.8.0-beta3"] ; http server
-                 [metosin/reitit "0.7.0-alpha7" :exclusions [metosin/malli]] ; Router for Clojure(Script)
-                 [metosin/malli "0.13.0"]
+                 [http-kit "2.8.0"] ; http server
+                 [metosin/reitit "0.7.0" :exclusions [metosin/malli]] ; Router for Clojure(Script)
+                 [metosin/malli "0.16.1"]
                  [metosin/ring-http-response "0.9.3" :exclusions [ring/ring-core]] ; No magic numbers for http statuses
                  [prestancedesign/get-port "0.1.1"] ; Get an available TCP port
 
                  ;; API security, authorization, authentication
                  [ovotech/ring-jwt "2.3.0" :exclusions [cheshire]] ; authentication, authorization based on jwt, exclude vulnerable transitive dependency [com.fasterxml.jackson.dataformat/jackson-dataformat-cbor "2.10.2"]
-                 [cheshire "5.12.0"]
+                 [cheshire "5.13.0"]
 
                  ;; Kafka and avro
                  [org.apache.kafka/kafka-clients "7.5.1-ce"]
@@ -34,19 +34,19 @@
 
                  ;; Configuration
                  [com.brunobonacci/oneconfig "0.22.0" :exclusions [metosin/jsonista com.cognitect.aws/api]] ; Exclude vulnerable transitive dependency [org.eclipse.jetty/jetty-io "9.4.24.v20191120"]
-                 [com.cognitect.aws/api "0.8.686"] ; Hard-patch vulnerable transitive dependency
+                 [com.cognitect.aws/api "0.8.692"] ; Hard-patch vulnerable transitive dependency
                  [piotr-yuxuan/closeable-map "0.36.2"] ; A Clojure map that implements java.io.Closeable
                  [com.github.piotr-yuxuan/malli-cli "2.1.2"] ; Configuration value from the command-line
 
-                 [babashka/process "0.5.21"] ; Clojure wrapper for java.lang.ProcessBuilder
+                 [babashka/process "0.5.22"] ; Clojure wrapper for java.lang.ProcessBuilder
                  [juji/editscript "0.6.3"] ; A diff library for Clojure/ClojureScript data structures
                  [camel-snake-kebab "0.4.3"] ; case manipulation
-                 [com.xtdb/xtdb-core "1.24.1" :exclusions [org.clojure/data.json org.clojure/tools.reader]] ; Database
+                 [com.xtdb/xtdb-core "1.24.3" :exclusions [org.clojure/data.json org.clojure/tools.reader]] ; Database
 
                  ;; Language constructs and utilities
-                 [org.clojure/clojure "1.12.0-alpha4"] ; The language itself
-                 [org.clojure/core.cache "1.0.225"] ; Caching logic
-                 [org.clojure/data.avl "0.1.0"]
+                 [org.clojure/clojure "1.12.0-alpha12"] ; The language itself
+                 [org.clojure/core.cache "1.1.234"] ; Caching logic
+                 [org.clojure/data.avl "0.2.0"]
                  ]
   :main piotr-yuxuan.welcome-base-api.main
   :profiles {:github {:github/topics ["clojure" "api" "example" "exchange" "finance" "fintech" "market"
@@ -62,10 +62,10 @@
                    :dependencies [[ring/ring-devel "2.0.0-alpha-1" :exclusions [crypto-random commons-io]] ; `wrap-reload` ring middleware
                                   [criterium "0.4.6"] ; Basic performance test. Use clojure-jmh for repeatable mesurements.
                                   [jmh-clojure "0.4.1"] ; Clojure wrapper for Java Microbenchmark Harness.
-                                  [com.clojure-goes-fast/clj-async-profiler "1.0.5"] ; Sampling CPU and HEAP profiler for Clojure featuring AsyncGetCallTrace + perf_events
+                                  [com.clojure-goes-fast/clj-async-profiler "1.2.2"] ; Sampling CPU and HEAP profiler for Clojure featuring AsyncGetCallTrace + perf_events
                                   [fipp "0.6.26"] ; Fast Idiomatic Pretty Printer for Clojure
                                   ]}
-             :kaocha {:dependencies [[lambdaisland/kaocha "1.87.1366"]]}
+             :kaocha {:dependencies [[lambdaisland/kaocha "1.91.1392"]]}
              :uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                                   "-Dclojure.compiler.direct-linking=true"]}}
